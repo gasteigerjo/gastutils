3c55f1372f7e3d62074722fa77d875a55fc2dd09
Johannes Klicpera <johannes.klicpera@tum.de>
2019-02-20 11:00:47 +0100

    Add adaptive matrix_to_tensor and mixed_dropout functions

--- jkutils/tensorflow.py
+++ jkutils/tensorflow.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import scipy.sparse as sp
 
 
 def sparse_matrix_to_tensor(X):
@@ -11,6 +12,20 @@ def sparse_matrix_to_tensor(X):
             coo.shape)
 
 
+def matrix_to_tensor(X):
+    if sp.issparse(X):
+        return sparse_matrix_to_tensor(X)
+    else:
+        return tf.constant(X, dtype=tf.float32)
+
+
 def sparse_dropout(X, dropout_rate):
     X_drop_val = tf.nn.dropout(X.values, dropout_rate)
     return tf.SparseTensor(X.indices, X_drop_val, X.dense_shape)
+
+
+def mixed_dropout(X, keep_prob):
+    if isinstance(X, tf.SparseTensor):
+        return sparse_dropout(X, keep_prob)
+    else:
+        return tf.nn.dropout(X, keep_prob)
