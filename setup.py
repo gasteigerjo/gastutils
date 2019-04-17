6f63d5c63add02f059aed79684048d410e20b7d8
Johannes Klicpera <johannes.klicpera@tum.de>
2019-04-17 13:43:50 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='jkutils',
+      version='0.1.0',
+      description='Joyful & kind utilities',
+      url='https://gitlab.lrz.de/klicperajo/jkutils',
+      author='Johannes Klicpera',
+      author_email='klicpera@in.tum.de',
+      packages=find_packages('.'),
+      zip_safe=False)
