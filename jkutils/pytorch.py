a6df52d73b9c74970f307b0e6998c65e47ed549c
Johannes Klicpera <klicpera@in.tum.de>
2021-07-05 02:34:06 +0200

    Sinkhorn; PyTorch convenience functions

--- jkutils/pytorch.py
+++ jkutils/pytorch.py
@@ -101,3 +101,81 @@ class Lambda(nn.Module):
 
     def forward(self, x):
         return self.func(x)
+
+
+def logcosh(x):
+    return x + F.softplus(-2. * x) - math.log(2.)
+
+
+def trace_batch(mat):
+    return torch.diagonal(mat, dim1=-2, dim2=-1).sum(-1)
+
+
+def max_finite(tensor, dim=None):
+    mask = torch.isinf(tensor)
+    tensor_neginf = torch.masked_fill(tensor, mask, -math.inf)
+    if dim is None:
+        return tensor_neginf.max()
+    else:
+        return tensor_neginf.max(dim=dim)
+
+
+@torch.jit.script
+def sum_finite(tensor: torch.Tensor, dim: int):
+    mask = ~torch.isfinite(tensor)
+    tensor_finite = torch.masked_fill(tensor, mask, 0)
+    return torch.sum(tensor_finite, dim)
+
+
+def sinkhorn_normalization(mat: torch.FloatTensor, niter: int, mean_one: bool = False, mask: torch.BoolTensor = None):
+    batch_size, nrows, ncols = mat.shape
+    assert nrows == ncols
+
+    if mean_one:
+        target_sum = nrows
+    else:
+        target_sum = 1
+
+    # Mask for padded tensors
+    if mask is not None:
+        mat_u = mat.clone().transpose(1, 2)
+        mat_u[:, :, 0] += mask
+
+        mat_v = mat.clone()
+        mat_v[:, :, 0] += mask
+
+    u = mat.new_ones([batch_size, nrows])
+    for _ in range(niter):
+        v = torch.clamp(target_sum / torch.einsum("bij, bj -> bi", mat_u, u), max=1e10)
+        u = torch.clamp(target_sum / torch.einsum("bij, bj -> bi", mat_v, v), max=1e10)
+
+    return u, v
+
+
+def logsinkhorn_normalization(mat: torch.FloatTensor, niter: int, mean_one: bool):
+    batch_size, nrows, ncols = mat.shape
+    assert nrows == ncols
+
+    if mean_one:
+        target_sum = math.log(nrows)
+    else:
+        target_sum = 0
+
+    def M(u, v):
+        "Modified cost for logarithmic updates"
+        "$M_{ij} = -c_{ij} + u_i + v_j$"
+        # clamp to prevent NaN for inf - inf
+        if u is None:
+            return v[:, None, :] + mat
+        elif v is None:
+            return u[:, :, None] + mat
+        else:
+            return u[:, :, None] + v[:, None, :] + mat
+
+    u = mat.new_zeros(batch_size, nrows)
+    v = mat.new_zeros(batch_size, nrows)
+    for _ in range(niter):
+        u = target_sum - torch.logsumexp(M(None, v), dim=-1)
+        v = target_sum - torch.logsumexp(M(u, None), dim=-2)
+
+    return u, v
