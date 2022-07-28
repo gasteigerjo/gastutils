b7d10a1213914b060f3d34bcec7b5cd68ed3bf39
Johannes Gasteiger <gasteiger@mailbox.org>
2022-07-28 21:04:19 +0200

    Rename and move to Github

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ install_requires = [
     "matplotlib>=3.3"
 ]
 
-setup(name='jkutils',
+setup(name='gastutils',
       version='0.1.0',
-      description='Joyful & kind utilities',
-      url='https://gitlab.lrz.de/klicperajo/jkutils',
-      author='Johannes Klicpera',
-      author_email='klicpera@in.tum.de',
+      description='Gasteiger utilities',
+      url='https://github.com/gasteigerjo/gastutils',
+      author='Johannes Gasteiger',
+      author_email='gasteiger@mailbox.org',
       packages=find_packages('.'),
       install_requires=install_requires,
       python_requires='>=3.6',
